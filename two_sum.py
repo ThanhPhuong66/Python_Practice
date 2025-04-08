97227580f087c127c107dccf6dfcf13078d35cca
Le Thi Thanh Phuong <phuongle662002@gmail.com>
2025-04-08 15:43:32 +0700

    #1 [Bai_1 two_sum] Thêm comment yêu cầu của bài vào file

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+from typing import List  
+
+class Solution:  
+    def twoSum(self, nums: List[int], target: int) -> List[int]:  
+        num_to_index = {}
+
+        for index, num in enumerate(nums): 
+            complement = target - num  
+            if complement in num_to_index:  
+                return [num_to_index[complement], index]  
+            num_to_index[num] = index  
+        return [] 
+
+
+solution = Solution()  
+nums = [2,3,5,7,9]  
+target = 14
+result = solution.twoSum(nums,target)  
+print(result) 
