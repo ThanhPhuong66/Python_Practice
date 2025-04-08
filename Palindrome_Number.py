fb94f99b2503895c0ec35a733d0a4b51f0b42f68
Le Thi Thanh Phuong <phuongle662002@gmail.com>
2025-04-08 15:31:22 +0700

    #3 - [bài_2 palindrome_number] Thêm comment yêu cầu của bài vào file

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,3 +1,9 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+# Example 1:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
