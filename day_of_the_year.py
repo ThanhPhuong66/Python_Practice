0f925918c17f635e7f498a87ebc8f8656181eaf8
Le Thi Thanh Phuong <phuongle662002@gmail.com>
2025-04-08 15:41:49 +0700

    #4 [bai_4 day_of_the_year] Thêm yêu cầu của bài vào file

--- day_of_the_year.py
+++ day_of_the_year.py
@@ -3,6 +3,7 @@
 # Output: 9
 # Explanation: Given date is the 9th day of the year in 2019.
 
+
 class Solution:  
     def dayOfYear(self, date: str) -> int:  
         year, month, day = map(int, date.split('-'))  
